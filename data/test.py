f84483edb769c8233bfe225582506b2efeae8e94
nikniksham <nikniksham@gmail.com>
2020-04-30 13:59:48 +0300

    fix test

--- data/test.py
+++ data/test.py
@@ -1,11 +1,10 @@
 from requests import get, post, delete, put
 
-test_user_version_first = False
-test_user_version_second = False
-test_developers_diary_version_first = False
-test_developers_diary_version_second = False
-test_publication_version_first = False
-test_publication_version_second = False
+test_user_version_first = True
+test_user_version_second = True
+test_developers_diary_version_first = True
+test_publication_version_first = True
+test_publication_version_second = True
 
 link_website = 'https://rjkzavrs-studio.herokuapp.com/'
 link_website_2 = 'http://localhost:8000/'
@@ -17,7 +16,7 @@ if test_user_version_first:
     if test_post:
         print(get(f'{link_website_2}api/user/test_account@yandex.ru/test_123').json())
         print(post(f'{link_website_2}api/user',
-                   json={'id': 10,
+                   json={'id': 100,
                          'name': 'Николай',
                          'surname': 'Шамков',
                          'age': 16,
@@ -26,7 +25,7 @@ if test_user_version_first:
                          'password': 'test_123'}).json())
 
     if test_get:
-        print(get(f'{link_website_2}api/user/test_account@yandex.ru/test_123/1').json())
+        print(get(f'{link_website_2}api/user/test_account@yandex.ru/test_123').json())
 
     if test_put:
         print(put(f'{link_website_2}api/user/test_account@yandex.ru/test_123', json={'age': 20}).json())
@@ -37,43 +36,22 @@ if test_user_version_first:
 
 if test_user_version_second:
     if test_get:
-        print(get(f'{link_website_2}api/users/test@yandex.ru/test_123').json())
+        print(get(f'{link_website_2}api/users/test@yandex.ru/test_123/5').json())
 
     if test_put:
-        print(put(f'{link_website_2}api/user/test@yandex.ru/test_123/3', json={'age': 15}).json())
-        print(get(f'{link_website_2}api/user/test@yandex.ru/test_123/3').json())
+        print(put(f'{link_website_2}api/user/test@yandex.ru/test_123/5', json={'age': 15}).json())
+        print(get(f'{link_website_2}api/user/test@yandex.ru/test_123/5').json())
 
     if test_delete:
-        print(delete(f'{link_website_2}api/user/test@yandex.ru/test_123/3').json())
-        print(get(f'{link_website_2}api/user/test@yandex.ru/test_123/3').json())
+        print(delete(f'{link_website_2}api/user/test@yandex.ru/test_123/5').json())
+        print(get(f'{link_website_2}api/user/test@yandex.ru/test_123/5').json())
+
 
 if test_developers_diary_version_first:
     if test_post:
-        print(get(f'{link_website_2}api/developers_diary/test_account@yandex.ru/test_123/1').json())
-        print(post(f'{link_website_2}api/developers_diary_admin_create/test_account@yandex.ru/test_123',
-                   json={'id': 10,
-                         'header': 'Тестовая запись 2',
-                         'body': 'Тестовая запись с тестовой информацией.'
-                                 'Тестовая информация для тестовой записи',
-                         'availability_status': 1}).json())
-
-    if test_get:
-        print(get(f'{link_website_2}api/developers_diary/test_account@yandex.ru/test_123/10').json())
-
-    if test_put:
-        print(put(f'{link_website_2}api/developers_diary/test_account@yandex.ru/test_123/10',
-                  json={'availability_status': 0}).json())
-        print(get(f'{link_website_2}api/developers_diary/test_account@yandex.ru/test_123/10').json())
-
-    if test_delete:
-        print(delete(f'{link_website_2}api/developers_diary/test_account@yandex.ru/test_123/10').json())
-        print(get(f'{link_website_2}api/developers_diary/test_account@yandex.ru/test_123/10').json())
-
-if test_developers_diary_version_second:
-    if test_post:
-        print(get(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/10').json())
+        print(get(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/100').json())
         print(post(f'{link_website_2}api/developers_diary_admin_create/test@yandex.ru/test_123',
-                   json={'id': 10,
+                   json={'id': 100,
                          'header': 'Тестовая запись 2',
                          'body': 'Тестовая запись с тестовой информацией.'
                                  'Тестовая информация для тестовой записи',
@@ -81,56 +59,57 @@ if test_developers_diary_version_second:
 
     if test_get:
         print(get(f'{link_website_2}api/developers_diary_admin_list/test@yandex.ru/test_123').json())
-        print(get(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/10').json())
+        print(get(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/100').json())
 
     if test_put:
-        print(put(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/10',
+        print(put(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/100',
                   json={'header': 'Тестовая смена названия 1'}).json())
-        print(get(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/10').json())
+        print(get(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/100').json())
 
     if test_delete:
-        print(delete(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/10').json())
-        print(get(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/10').json())
+        print(delete(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/100').json())
+        print(get(f'{link_website_2}api/developers_diary_admin/test@yandex.ru/test_123/100').json())
 
 
 if test_publication_version_first:
     if test_post:
-        print(get(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/10').json())
+        print(get(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/100').json())
         print(post(f'{link_website_2}api/publication_create/test_account@yandex.ru/test_123',
-                   json={'id': 10,
+                   json={'id': 100,
                          'header': 'Тестовая запись',
                          'body': 'Тестовая запись с тестовой информацией.'
                                  'Тестовая информация для тестовой записи'}).json())
 
     if test_get:
-        print(get(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/10').json())
+        print(get(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/100').json())
 
     if test_put:
-        print(put(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/10',
+        print(put(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/100',
                   json={'header': 'Тестовая смена загаловка'}).json())
-        print(get(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/10').json())
+        print(get(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/100').json())
 
     if test_delete:
-        print(delete(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/10').json())
-        print(get(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/10').json())
+        print(delete(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/100').json())
+        print(get(f'{link_website_2}api/publication/test_account@yandex.ru/test_123/100').json())
+
 
 if test_publication_version_second:
     if test_post:
-        print(get(f'{link_website_2}api/publication/test@yandex.ru/test_123/11').json())
+        print(get(f'{link_website_2}api/publication/test@yandex.ru/test_123/100').json())
         print(post(f'{link_website_2}api/publication_create/test@yandex.ru/test_123',
-                   json={'id': 11,
+                   json={'id': 100,
                          'header': 'Тестовая запись',
                          'body': 'Тестовая запись с тестовой информацией.'
                                  'Тестовая информация для тестовой записи'}).json())
 
     if test_get:
-        print(get(f'{link_website_2}api/publication/test@yandex.ru/test_123/11').json())
+        print(get(f'{link_website_2}api/publication/test@yandex.ru/test_123/100').json())
 
     if test_put:
-        print(put(f'{link_website_2}api/publication/test@yandex.ru/test_123/11',
+        print(put(f'{link_website_2}api/publication/test@yandex.ru/test_123/100',
                   json={'header': 'Тестовая смена загаловка'}).json())
-        print(get(f'{link_website_2}api/publication/test@yandex.ru/test_123/11').json())
+        print(get(f'{link_website_2}api/publication/test@yandex.ru/test_123/100').json())
 
     if test_delete:
-        print(delete(f'{link_website_2}api/publication/test@yandex.ru/test_123/11').json())
-        print(get(f'{link_website_2}api/publication/test@yandex.ru/test_123/11').json())
+        print(delete(f'{link_website_2}api/publication/test@yandex.ru/test_123/100').json())
+        print(get(f'{link_website_2}api/publication/test@yandex.ru/test_123/100').json())
